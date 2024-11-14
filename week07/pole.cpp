f70ead0e122eab32ea2255a0f1e63b85a2401cae
Šimon Nedorostek <simon.nedorostek@gmail.com>
2024-11-14 17:30:32 +0100

    a

--- week07/pole.cpp
+++ week07/pole.cpp
@@ -2,13 +2,18 @@
 #include <vector>
 
 void print(std::vector<int> a) {
+    std::cout << "[";
     for (std::size_t i = 0; i < a.size();i++) {
-        std::cout << a[i] << std::endl;
+        if (i != 0) { 
+            std::cout << ", ";
+        }
+        std::cout << a[i];
     }
+    std::cout << "]" << std::endl;
 }
 
 int main () {
-    std::vector<int> pole = {1, 2, 3};
+    std::vector<int> pole = {4, 8, 15, 16, 23, 42};
 
     //for (std::size_t i = 0; i < pole.size();i++) {
     //    std::cout << pole[i] << std::endl;
