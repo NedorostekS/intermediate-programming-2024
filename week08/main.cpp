e8d9bd27ebf736038c6e83f1656f5026c0960258
Šimon Nedorostek <simon.nedorostek@gmail.com>
2024-11-21 18:00:52 +0100

    main done

--- week08/main.cpp
+++ week08/main.cpp
@@ -16,37 +16,100 @@ void print(std::vector<int> data) {
 }
 
 int sum(std::vector<int> data) {
-    return 0;
+    int a = 0;
+    for (std::size_t i = 0; i < data.size(); i++) {
+        int now = data[i];
+        a = a + now;
+    }
+
+    //std::cout << a << std::endl;
+
+    return a;
 }
 
 float average(std::vector<int> data) {
-    return 0.0;
+    float a = 0;
+    for (std::size_t i = 0; i < data.size(); i++) {
+        float now = data[i];
+        a = a + now;
+    }
+    
+    float res = a / data.size();
+    
+    return res;
 }
+
 
 float median(std::vector<int> data) {
-    return 0.0;
+    float i = 0;
+
+    if (((data.size() + 1) % 2) ==  1) {
+        // even
+        i = data.size() / 2;
+        i = data[i] + data[i + 1];
+        i = i / 2;
+    } else {
+        // not even
+        i = data.size() / 2;
+        i = data[i];
+    }
+
+    return i;
 }
 
-std::vector<int> range(int start, int end) {
-    return {};
+std::vector<int> range(int start, int end) { 
+    std::vector<int> range_pole = {};
+    if (start < end) {
+        for (int i = start; i < end; i++ ) {
+        range_pole.push_back(i);
+        }
+    } else if (start > end) {
+        for (int i = start; i > end; i-- ) { 
+        range_pole.push_back(i);
+        }
+    } else {
+    }
+
+    return range_pole;
 }
 
 std::vector<int> runningSum(std::vector<int> data) {
-    return {};
+    std::vector<int> runningSum_array = {};
+    int a = 0;
+    for (std::size_t i = 0; i < data.size(); i++) {
+        int now = data[i];
+        a = a + now;
+        runningSum_array.push_back(a);
+    }
+
+    return runningSum_array;
 }
 
 std::vector<int> rotateRight(std::vector<int> data, int count) {
-    return data;
+    std::vector<int> rotate_array = {};
+
+    count = count % data.size();
+
+    for (std::size_t i = count; i < data.size(); i++) {
+        //std::cout << i << std::endl;
+        rotate_array.push_back(data[i]);
+    }
+    for (int i = 0; i < count; i++) {
+        rotate_array.push_back(data[i]);
+    }
+
+    return rotate_array;
 }
+
 
 int main() {
     std::vector<int> vec1 = { 1, 9, 4, 8, 4, 0, 9, 2, 3, 5, 6 };
-    std::vector<int> asc1 = { 0, 2, 4, 7, 8 };
+    std::vector<int> asc1 = { 0, 2, 4, 7, 8};
     std::vector<int> asc2 = { 1, 3, 5, 6, 7, 9 };
 
     std::cout << "sum(vec1): " << sum(vec1) << std::endl;
     std::cout << "average(vec1): " << average(vec1) << std::endl;
-    std::cout << "median(asc1): " << median(vec1) << std::endl;
+    std::cout << "median(asc1): " << median(asc1) << std::endl;
 
     std::cout << "range(5, 10): ";
     print(range(5, 10));
@@ -59,4 +122,9 @@ int main() {
     std::cout << "runningSum(vec1): ";
     print(runningSum(vec1));
     std::cout << std::endl;
+
+    std::cout << "rotateRight: ";
+    print(rotateRight(vec1, 3));
+    std::cout << std::endl;
+
 }
